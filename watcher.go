9406bc5c8daf368c4ede5c201d804213638e55be
CE-Matthias Kihr <ce-matthias.kihr@regnology.net>
2025-07-02 10:10:44 +0200

    fixed go and test

--- watcher.go
+++ watcher.go
@@ -8,12 +8,12 @@ import (
 	"strings"
 	"time"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/api/core/v1"
-	"k8s.io/api/apps/v1"
 )
 
 type KubeClient interface {
--- watcher_test.go
+++ watcher_test.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"testing"
 
-	corev1 "k8s.io/api/core/v1"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // MockKubeClient implements the KubeClient interface for testing
